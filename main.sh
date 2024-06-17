b1b6d455b14c4dbe7047cb2e23e107597ff49c69
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-16 20:02:44 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -50,17 +50,17 @@ shift $((OPTIND -1))
 # Use all tools to crawl domain and add to a file
 crawler() {
   # Run hakrawler and add output to a file
-  cat domain | hakrawler | tee -a bigCrawl &
+  cat $domain | hakrawler -i -d 5 | tee -a bigCrawl &
 
   # Run getallurls and add output to a list
-  cat domain | getallurls -threads 8 | tee -a bigCrawl &
+  cat $domain | gau | tee -a bigCrawl &
 
   # Run waybackurls and add output to a list   
   #waybackList=$(waybackurls $domain)
-  cat domain | waybackurls | tee -a bigCrawl &
+  cat $domain | waybackurls | tee -a bigCrawl &
 
   # Run katana and add to file
-  katana -list subdomains | tee -a bigCrawl &
+  #katana -list subdomains | tee -a bigCrawl &
 
   # Wait for all processes to finish
   wait
@@ -71,18 +71,21 @@ crawler() {
 fuzzer() {
   # Check if -f option is provided
   if [ "$fuzzOption" = true ]; then
+
+    echo "Please enter domain to fuzz:"
+    read name
+    # Create list for fuzzing
+    cat bigCrawl | grep = | grep ? | grep $name | uro | tee keyFuzz
+    sed -i 's/\?\([^=]*\)=/?FUZZ=/g' keyFuzz
+    sed -i 's/=\(.*\)/=readme/g' keyFuzz
+
     # Run ffuf against the list of domains for directories and add to a file
-    ffuf -c -w "/path/to/wordlist.txt" -maxtime-job 60 -recursion -recursion-depth 5 -u $domain/FUZZ | tee -a bigCrawl
+    cat keyFuzz | while read url ; do ffuf -w ~/Downloads/fuzz-Bo0oM.txt -u "$url"; done
   else
     echo "Skipping fuzzing"
   fi
 }
 
-# Clean up directory list
-cleaner() {
-  
-
-}
 
 # Main function to call other functions
 main() {
@@ -91,3 +94,4 @@ main() {
 }
 
 main
+
