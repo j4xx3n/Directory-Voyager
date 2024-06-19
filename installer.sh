a0d4a62d172e6507f68c4fdd730e85abba4e2b88
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-18 22:47:55 -0500

    Update and rename installer.sh to kali-installer.sh

--- installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt install golang-go -y
-go install github.com/hakluke/hakrawler@latest
-go install github.com/lc/gau/v2/cmd/gau@latest
-go install github.com/tomnomnom/waybackurls@latest
-#go install github.com/projectdiscovery/katana/cmd/katana@latest
-go install github.com/ffuf/ffuf/v2@latest
-cp ~/go/bin/* /usr/bin/
