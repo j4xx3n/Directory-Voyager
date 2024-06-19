5eb0c5189e24519d00f63b0fc4c18bc33f4b3d89
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-19 11:06:43 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,9 +11,12 @@ echo "By: J4xx3n"
 subdomains=""
 passive=false
 active=false
+fuzz=false
+clean=false
+cleanArg=""
 
 # Parse command-line options
-while getopts ":s:pa" opt; do
+while getopts ":s:pafc" opt; do
   case ${opt} in
     s )
       subdomains="$OPTARG"
@@ -23,6 +26,13 @@ while getopts ":s:pa" opt; do
       ;;
     a )
       active=true
+      ;;
+    f )
+      fuzz=true
+      ;;
+    c )
+      cleanArg="$OPTARG"
+      clean=true
       ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
@@ -37,43 +47,46 @@ done
 shift $((OPTIND -1))
 
 
-
-# Use all tools to crawl domain and add to a file
 passiveScan() {
-  # Run
-  hakrawler and add output to a file
-  cat subdomains | hakrawler -i -d 5 | tee -a bigCrawl &
-
   # Run getallurls and add output to a list
-  cat subdomains | gau | tee -a bigCrawl &
+  cat $subdomains | getallurls | tee -a bigCrawl
 
   # Run waybackurls and add output to a list
-  #waybackList=$(waybackurls $domain)
-  cat subdomains | waybackurls | tee -a bigCrawl &
+  cat $subdomains | waybackurls | tee -a bigCrawl
 
-  # Run katana and add to file
-  #katana -list subdomains | tee -a bigCrawl &
-
-  # Wait for all processes to finish
-  wait
+  # Run katana passively and add to file
+  katana -list $subdomains -d 5 -ps | tee -a bigCrawl
 }
 
 
-# Fuzz domain list for paths
 activeScan() {
+  # Run hakrawler and add output to a file
+  cat $subdomains | hakrawler -d 5 | tee -a bigCrawl
 
-  # Fuzz domain list for paths
-  echo "Please enter domain to fuzz:"
-  read name
-  # Create list for fuzzing
-  cat bigCrawl | grep = | grep ? | grep $name | uro | tee keyFuzz
-  sed -i 's/\?\([^=]*\)=/?FUZZ=/g' keyFuzz
-  sed -i 's/=\(.*\)/=readme/g' keyFuzz
-  # Run ffuf against the list of domains for directories and add to a file
-  cat keyFuzz | while read url ; do ffuf -w ~/Downloads/fuzz-Bo0oM.txt -u "$url"; done
-
+  # Run katana actively and add to file
+  katana -list $subdomains -d 5 | tee -a bigCrawl
 }
 
+
+fuzzScan(){
+  # Fuzz domain list for paths
+  #echo "Please enter domain to fuzz:"
+  echo "Fuzz function out of commition."
+  #read name
+  # Create list for fuzzing
+  #cat bigCrawl | grep = | grep ? | grep $name | uro | tee keyFuzz
+  #sed -i 's/\?\([^=]*\)=/?FUZZ=/g' keyFuzz
+  #sed -i 's/=\(.*\)/=readme/g' keyFuzz
+  # Run ffuf against the list of domains for directories and add to a file
+  #cat keyFuzz | while read url ; do ffuf -w ~/Downloads/fuzz-Bo0oM.txt -u "$url"; done
+}
+
+cleanList() {
+  echo "Clen function is out of commition."
+  # Sort out duplicates and urls not in scope
+  #cat bigCrawl | grep $cleanArg | sort -u | tee bigCrawl
+  #echo "$cleanArg"
+}
 
 # Main function to call other functions
 main() {
@@ -83,6 +96,14 @@ main() {
 
   if [ "$active" = true ]; then
     activeScan
+  fi
+
+  if [ "$fuzz" = true ]; then
+    fuzzScan
+  fi
+
+  if [ "$clean" = true ]; then
+    cleanList
   fi
 }
 
