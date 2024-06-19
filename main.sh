93519ae781f3e8b4ce084222a9f469d44d22d287
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-18 22:47:13 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,35 +3,26 @@
 # Wrapper for the most reputable domain scanners to create one large list
 # Crawlers to use: hakrawler, getallurls, waybackurls
 
-# ASCII title
-echo "
-                              
-@@@  @@@  @@@  @@@  @@@@@@@@  
-@@@  @@@  @@@@ @@@  @@@@@@@@  
-@@!  !@@  @@!@!@@@       @@!  
-!@!  @!!  !@!!@!@!      !@!   
-@!@@!@!   @!@ !!@!     @!!    
-!!@!!!    !@!  !!!    !!!     
-!!: :!!   !!:  !!!   !!:      
-:!:  !:!  :!:  !:!  :!:       
- ::  :::   ::   ::   ::       
- :   :::  ::    :   : :
-"
+
 echo "Domain crawler wrapper"
 echo "By: J4xx3n"
 
 # Create variable for target domain list
-domain=""
-fuzzOption=false
+subdomains=""
+passive=false
+active=false
 
 # Parse command-line options
-while getopts ":d:f" opt; do
+while getopts ":s:pa" opt; do
   case ${opt} in
-    d )
-      domain="$OPTARG"
+    s )
+      subdomains="$OPTARG"
       ;;
-    f )
-      fuzzOption=true
+    p )
+      passive=true
+      ;;
+    a )
+      active=true
       ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
@@ -48,16 +39,17 @@ shift $((OPTIND -1))
 
 
 # Use all tools to crawl domain and add to a file
-crawler() {
-  # Run hakrawler and add output to a file
-  cat $domain | hakrawler -i -d 5 | tee -a bigCrawl &
+passiveScan() {
+  # Run
+  hakrawler and add output to a file
+  cat subdomains | hakrawler -i -d 5 | tee -a bigCrawl &
 
   # Run getallurls and add output to a list
-  cat $domain | gau | tee -a bigCrawl &
+  cat subdomains | gau | tee -a bigCrawl &
 
-  # Run waybackurls and add output to a list   
+  # Run waybackurls and add output to a list
   #waybackList=$(waybackurls $domain)
-  cat $domain | waybackurls | tee -a bigCrawl &
+  cat subdomains | waybackurls | tee -a bigCrawl &
 
   # Run katana and add to file
   #katana -list subdomains | tee -a bigCrawl &
@@ -68,30 +60,30 @@ crawler() {
 
 
 # Fuzz domain list for paths
-fuzzer() {
-  # Check if -f option is provided
-  if [ "$fuzzOption" = true ]; then
+activeScan() {
 
-    echo "Please enter domain to fuzz:"
-    read name
-    # Create list for fuzzing
-    cat bigCrawl | grep = | grep ? | grep $name | uro | tee keyFuzz
-    sed -i 's/\?\([^=]*\)=/?FUZZ=/g' keyFuzz
-    sed -i 's/=\(.*\)/=readme/g' keyFuzz
+  # Fuzz domain list for paths
+  echo "Please enter domain to fuzz:"
+  read name
+  # Create list for fuzzing
+  cat bigCrawl | grep = | grep ? | grep $name | uro | tee keyFuzz
+  sed -i 's/\?\([^=]*\)=/?FUZZ=/g' keyFuzz
+  sed -i 's/=\(.*\)/=readme/g' keyFuzz
+  # Run ffuf against the list of domains for directories and add to a file
+  cat keyFuzz | while read url ; do ffuf -w ~/Downloads/fuzz-Bo0oM.txt -u "$url"; done
 
-    # Run ffuf against the list of domains for directories and add to a file
-    cat keyFuzz | while read url ; do ffuf -w ~/Downloads/fuzz-Bo0oM.txt -u "$url"; done
-  else
-    echo "Skipping fuzzing"
-  fi
 }
 
 
 # Main function to call other functions
 main() {
-  crawler
-  fuzzer
+  if [ "$passive" = true ]; then
+    passiveScan
+  fi
+
+  if [ "$active" = true ]; then
+    activeScan
+  fi
 }
 
 main
-
